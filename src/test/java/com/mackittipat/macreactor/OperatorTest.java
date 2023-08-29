20b6488326333ccac2e72fc5da0f65a989b384fb
Mac <mac.kittipat@gmail.com>
2023-08-29 20:41:44 +0700

    Update comment

--- src/test/java/com/mackittipat/macreactor/OperatorTest.java
+++ src/test/java/com/mackittipat/macreactor/OperatorTest.java
@@ -57,12 +57,12 @@ public class OperatorTest {
                 Flux.just("1", "2", "3").delayElements(Duration.ofMillis(100)),
                 Flux.just("A", "B", "C").delayElements(Duration.ofMillis(250)));
         StepVerifier.create(result)
-                .expectNext("1")
-                .expectNext("2")
-                .expectNext("A")
-                .expectNext("3")
-                .expectNext("B")
-                .expectNext("C")
+                .expectNext("1") // 100
+                .expectNext("2") // 200
+                .expectNext("A") // 250
+                .expectNext("3") // 300
+                .expectNext("B") // 500
+                .expectNext("C") // 750
                 .expectComplete()
                 .verify();
     }
@@ -85,6 +85,7 @@ public class OperatorTest {
 
     @Test
     public void testThenMany() {
+        // Wait for Flux.just("1", "2", "3") to finish then publish Flux.just("A", "B", "C")
         Flux<String> result = Flux.just("1", "2", "3")
                 .thenMany(Flux.just("A", "B", "C"));
         StepVerifier.create(result)
@@ -99,6 +100,8 @@ public class OperatorTest {
     public void testDefer() {
         // All subscriber get same data
         // Math.random() is executed immediately
+        // Example : A radio station transmits a signal to its listeners.
+        // Hot Publishers do not create new data producer for each new subscription
         Flux<String> hotFlux = Flux.just(Math.random() + "");
         hotFlux.subscribe(System.out::println);
         hotFlux.subscribe(System.out::println);
@@ -106,6 +109,9 @@ public class OperatorTest {
         // All subscriber get difference data.
         // The defer operator is there to make this source lazy,
         // Math.random() is executed each time there is a new subscriber.
+        // Cold is default behavior
+        // Cold Publishers by default do not produce any value unless at least 1 observer subscribes to it.
+        // Publishers create new data producers for each new subscription.
         Flux<String> coldFlux = Flux.defer(() -> Flux.just(Math.random() + ""));
         coldFlux.subscribe(System.out::println);
         coldFlux.subscribe(System.out::println);
